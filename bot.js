4b178ca84c35f602d83daca9f1fa71b4cefacc9b
doniyorbeko1 <sharipovs374@gmail.com>
2026-05-25 16:38:22 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,28 @@ const adminId = 6964373903;
 const bot = new TelegramBot(token, { polling: true });
 
 console.log('Bot ishlayapti...');
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
 
+const app = express();
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+    bot.sendMessage(msg.chat.id, "Salom");
+});
+
+app.get("/", (req, res) => {
+    res.send("Bot ishlayapti");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
 
 // ======================
 // KARTALAR
